62a1cdf62eb448897bc734f32e7bc077a4e5efcb
domkow78 <dominik.kowalczyk@bshg.com>
2026-07-08 14:16:23 +0200

    update

--- lib/Board/Board.cpp
+++ lib/Board/Board.cpp
@@ -1,13 +1,5 @@
-#include <Arduino.h>
-
 #include "Board.h"
-#include "project.h"
-#include "Logger.h"
 
 void Board::init()
 {
-    pinMode(PIN_LED, OUTPUT);
-
-    Logger::begin(SERIAL_BAUDRATE);
-    Logger::info("Board initialized");
 }
--- src/main.cpp
+++ src/main.cpp
@@ -9,9 +9,4 @@ void setup()
 
 void loop()
 {
-    digitalWrite(PIN_LED, HIGH);
-    delay(500);
-
-    digitalWrite(PIN_LED, LOW);
-    delay(500);
 }
